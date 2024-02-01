8dc4f3a360a42adf2732682d9e541a2bbad3a1f2
ds-spyriadis <ds.spiriadis@gmail.com>
2024-02-01 13:07:18 +0200

    c90 alages sto arxeio confest pros8esa tous browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,70 +2,29 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):                 # psaxnei gia browser input
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
 @pytest.fixture(scope="class")
 def setup(request):                     #    ->   otan kanoume decrare ena fixture exoume kai ena request instance
-    driver = webdriver.Chrome()
+
+    browser_name = request.config.getoption("browser_name")    # eisagei ton browser sth metavlhth
+    if browser_name == "chrome":
+        driver = webdriver.Chrome()
+    elif browser_name == "edge":
+        driver = webdriver.Edge()
+    elif browser_name == "IE":
+        print("IE Driver")
+
+
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     request.cls.driver = driver         #    -> the driver sent to class object,
     yield                               # opote opoia klash xrhsimopoioi afto to fixture exei prosvash sto class driver (cls.driver)
     driver.close()
-    return driver
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import service
-#
-#
-#
-#
-# def pytest_addoption(parser):
-#     parser.addoption(
-#         "--browser_name", action="store", default="chrome"
-#     )
-#
-#
-# @pytest.fixture(scope="class")
-# def setup(request):
-#     browser_name = request.config.getoption("browser_name")
-#     if browser_name == "chrome":
-#         driver = webdriver.Chrome()
-#     elif browser_name == "edge":
-#         driver = webdriver.Edge()
-#     elif browser_name == "IE":
-#         print("IE Driver")
-#
-#     driver.get("https://rahulshettyacademy.com/angularpractice/")
-#     driver.maximize_window()
-#
-#
-#
-#     request.cls.driver = driver
-#     yield
-#     driver.close()
