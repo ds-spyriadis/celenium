ea22b2c76454cc31e648837051795dcbc42fa4a1
ds-spyriadis <ds.spiriadis@gmail.com>
2024-02-01 16:32:48 +0200

    c92 dhmiourgia CheckOutPage class

--- pageObjects/CheckoutPage.py
+++ pageObjects/CheckoutPage.py
@@ -1,25 +1,37 @@
-# from selenium.webdriver.common.by import By
-#
-#
-# class CheckOutPage:
-#     def __init__(self,driver):
-#         self.driver = driver
-#
-#     #cards = self.driver.find_elements(By.CSS_SELECTOR, ".card-title a")
-#     #self.driver.find_elements(By.CSS_SELECTOR, ".card-footer button")[i].click()
-#     #self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
-#
-#     cardTitle = (By.CSS_SELECTOR, ".card-title a")
-#     cardFooter =(By.CSS_SELECTOR, ".card-footer button")
-#     checkOut = (By.XPATH, "//button[@class='btn btn-success']")
-#
-#
-#     def getCardTitles(self):
-#         return self.driver.find_elements(*CheckOutPage.cardTitle) # epeidh eiani tupple vazw to asteraki gia na kanei diseirialized
-#
-#
-#     def getCardFooter(self):
-#         return self.driver.find_elements(*CheckOutPage.cardFooter)
-#
-#     def checkOutItems(self):
-#         return self.driver.find_elements(CheckOutPage.checkOut)
+from selenium.webdriver.common.by import By
+
+
+class CheckOutPage:
+    def __init__(self,driver):
+        self.driver = driver
+
+    #cards = self.driver.find_elements(By.CSS_SELECTOR, ".card-title a")
+    #self.driver.find_elements(By.CSS_SELECTOR, ".card-footer button")[i].click()
+    #self.driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
+
+    cardTitle = (By.CSS_SELECTOR, ".card-title a")
+    cardFooter = (By.CSS_SELECTOR, ".card-footer button")
+    checkOut = (By.XPATH, "//button[@class='btn btn-success']")
+    getButton = (By.CSS_SELECTOR, "a[class*='btn-primary']")
+    presentCountry = (By.ID, "country")
+    country = (By.LINK_TEXT, "India")
+
+
+
+    def getCardTitles(self):
+        return self.driver.find_elements(*CheckOutPage.cardTitle) # epeidh eiani tupple vazw to asteraki gia na kanei diserialized
+
+
+    def getCardFooter(self):
+        return self.driver.find_elements(*CheckOutPage.cardFooter)
+
+    def checkOutItems(self):
+        return self.driver.find_element(*CheckOutPage.checkOut)
+
+    def getItems(self):
+        return self.driver.find_element(*CheckOutPage.getButton)
+    def getPresentCountry(self):
+        return self.driver.find_element(*CheckOutPage.presentCountry)
+
+    def getCountry(self):
+        return self.driver.find_element(*CheckOutPage.country)
